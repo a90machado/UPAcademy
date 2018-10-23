381c855d0b95cfe61fadac0f62822ef66fe16ee7
a90machado <a90machado@gmail.com>
2018-10-23 16:14:13 +0100

    done

--- Aula 5/main.js
+++ Aula 5/main.js
@@ -1,25 +1,29 @@
 
 function checkInput(){
-    var ipt = "";
-    ipt = document.getElementById("input1").value;
-    console.log('str:',ipt,"length",ipt.length);
-    console.log(charCount(ipt));
+    var ipt1, ipt2 = "";
+    ipt1 = document.getElementById("input1").value;
+    ipt2 = document.getElementById("input2").value;
+    var count1 =charCount(ipt1);
+    var count2 =charCount(ipt2);
 
-    document.getElementById("result").innerHTML = charCount(ipt);
+    document.getElementById("result").innerHTML = getConteudo(count1,count2);
 }
 
 function charCount(userInput){
 
-    console.log(typeof userInput);
+    //console.log(typeof userInput);
     
     var count=0;
-    console.log('Oentrou str:', userInput,"length:",userInput.lenght);
     for (var x = 0; x < userInput.length; x++){
-        console.log('Ola');
         if (userInput[x]==" "){
-            console.log('IF');
             count++;
         }
     }
     return count;
+}
+
+
+function getConteudo(count1,count2){
+
+    return `<p>No primeiro estavam ${count1} espacos<br>No segundo estavam ${count2} espacos </p>`
 }
