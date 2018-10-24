658670f677ec2118bc8366f87ddf624517d6b39b
a90machado <a90machado@gmail.com>
2018-10-24 14:39:16 +0100

    comment

--- Home/js/ex1.js
+++ Home/js/ex1.js
@@ -1,8 +1,6 @@
 //button onclick
 function action(){
-
     var input = document.getElementById("userInput").value;
-
     //check if input is only numbers
     if (input.match(/^[0-9]+$/) != null){
         //only numbers
@@ -12,35 +10,27 @@ function action(){
         alert("You naughty! Only numbers!");
         document.getElementById("result").innerHTML = "Let's do it again!"
     }
-
 }
 
 //sort input
 function sortArray(string){
-
     //vars
     var array=string.split("");
     var output="";
     var temp, x, y;
-
     //order
     for (x = 0; x < array.length-1; x++){
-
         for (y = 0; y < array.length-1; y++){
-
             if(array[y] < array[y+1]){
-
                 temp = array[y];
                 array[y] = array[y+1];
                 array[y+1] = temp;
             }
         }
     }
-
     //prepare for submission
     for (x=0; x < array.length; x++){
         output += array[x];
     }
-
     return output;
 }
--- Home/js/ex2.js
+++ Home/js/ex2.js
@@ -1,8 +1,6 @@
 //button onclick
 function action(){
-
     var input = document.getElementById("userInput").value;
-
     //check if input is only numbers
     if (input.match(/^[0-9]+$/) != null){
         //only numbers
@@ -12,17 +10,14 @@ function action(){
         alert("You naughty! Only numbers!");
         document.getElementById("result").innerHTML = "Let's do it again!"
     }
-
 }
 
 //sort input
 function checkIt(string){
-
     //vars
     var array=string.split("");
     var output="";
     var i;
-
     //order
     for (i = 0; i < array.length-1; i++){
         if(parseInt(array[i]) % 2 == 0 && parseInt(array[i+1]) % 2 == 0){
@@ -32,11 +27,9 @@ function checkIt(string){
             array[i] = array[i]+"#"
         }
     }
-
     //prepare for submission
     for (x=0; x < array.length; x++){
         output += array[x];
     }
-
     return output;
 }
