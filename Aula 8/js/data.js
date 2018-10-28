6183ba33503e6b5bc4d1619211231249c66bdb51
a90machado <a90machado@gmail.com>
2018-10-28 14:15:54 +0000

    .

---
+++ Aula 8/js/data.js
@@ -0,0 +1,66 @@
+var data = [
+    {
+        id: 1,
+        nomePedido: "Ze Maria",
+        pedido: [{
+            nome: "Menu Tasty",
+            extra: "Cola sem gelo"
+        },
+        {
+            nome: "Menu BigMac"
+        },
+        {
+            nome: "Menu Bacon",
+            extra: "Cola sem gelo"
+        }]
+    },
+    {
+        id: 2,
+        nomePedido: "Nuno Louco",
+        pedido: [{
+            nome: "Menu Tasty",
+            extra: "Cola sem gelo"
+        },
+        {
+            nome: "Menu BigMac"
+        },
+        {
+            nome: "Menu Bacon",
+            extra: "Cola sem gelo"
+        }]
+    },
+    {
+        id: 3,
+        nomePedido: "Joao Trapalhao",
+        pedido: [{
+            nome: "Menu Tasty",
+            extra: "Cola sem gelo"
+        },
+        {
+            nome: "Menu BigMac"
+        },
+        {
+            nome: "Menu Bacon",
+            extra: "Cola sem gelo"
+        }]
+    },
+    {
+        id: 4,
+        nomePedido: "Andre Barnabé",
+        pedido: [{
+            nome: "Menu Tasty",
+            extra: "Cola sem gelo"
+        },
+        {
+            nome: "Menu BigMac"
+        }]
+    },
+    {
+        id: 5,
+        nomePedido: "David Golias",
+        pedido: [{
+            nome: "Menu Tasty",
+            extra: "Cola sem gelo"
+        }]
+    }
+]
--- Aula 8/js/ex1.js
+++ Aula 8/js/ex1.js
@@ -1,91 +1,20 @@
 function main() {
-
-    var data = [
-        {
-            id: 1,
-            nomePedido: "Ze Maria",
-            pedido: [{
-                nome: "Menu Tasty",
-                extra: "Cola sem gelo"
-            },
-            {
-                nome: "Menu BigMac"
-            },
-            {
-                nome: "Menu Bacon",
-                extra: "Cola sem gelo"
-            }]
-        },
-        {
-            id: 2,
-            nomePedido: "Nuno Louco",
-            pedido: [{
-                nome: "Menu Tasty",
-                extra: "Cola sem gelo"
-            },
-            {
-                nome: "Menu BigMac"
-            },
-            {
-                nome: "Menu Bacon",
-                extra: "Cola sem gelo"
-            }]
-        },
-        {
-            id: 3,
-            nomePedido: "Joao Trapalhao",
-            pedido: [{
-                nome: "Menu Tasty",
-                extra: "Cola sem gelo"
-            },
-            {
-                nome: "Menu BigMac"
-            },
-            {
-                nome: "Menu Bacon",
-                extra: "Cola sem gelo"
-            }]
-        },
-        {
-            id: 4,
-            nomePedido: "Andre Barnabé",
-            pedido: [{
-                nome: "Menu Tasty",
-                extra: "Cola sem gelo"
-            },
-            {
-                nome: "Menu BigMac"
-            }]
-        },
-        {
-            id: 5,
-            nomePedido: "David Golias",
-            pedido: [{
-                nome: "Menu Tasty",
-                extra: "Cola sem gelo"
-            }]
-        }
-    ]
     checkSpecialOrders(data);
 }
 
 
 
-function checkSpecialOrders(hamburger) {
+function checkSpecialOrders(data) {
     var specialOrders = [];
-
-    for (let index = 0; index < hamburger.length; index++) {
-        const order = hamburger[index];
-
+    for (let index = 0; index < data.length; index++) {
+        const order = data[index];
         for (let index = 0; index < order.pedido.length; index++) {
             const element = order.pedido[index];
-
             if (Object.keys(element).length > 1 && specialOrders.indexOf(order) == -1) {
                 specialOrders.push(order);
             }
         }
     }
     return console.log(specialOrders);
-    
 }
 
